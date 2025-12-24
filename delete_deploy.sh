c6028ebe7b46caf94c03b8bfb459412a9caeb8a9
Hemant Gupta <49529679+hemant709@users.noreply.github.com>
2025-12-24 11:05:22 +0000

    path modifications

--- delete_deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Enter the namespace name where the deployment is:"
-read NAMESPACE
-
-kubectl delete deployment hemant-deployment -n $NAMESPACE
-
-echo "Deployment hemant-deployment deleted from namespace $NAMESPACE."
